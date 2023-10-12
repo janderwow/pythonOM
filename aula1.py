590de76e36f5dc283acc3ad41553541c9bf4b902
Jander Melo <janderwow@gmail.com>
2023-10-12 08:26:20 -0300

    commit inicial

---
+++ aula1.py
@@ -0,0 +1,8 @@
+"""Docstrings como comentarios
+"""
+'''
+Tambem e uma Docstring
+'''
+
+# Permite escrever um comentario
+print(123)
---
+++ aula2.py
@@ -0,0 +1,4 @@
+#strings e tipos de string
+print(123,456,sep='-')
+print(789,10,11,12,sep='_')
+print(9,11,sep='\n',end='##')
