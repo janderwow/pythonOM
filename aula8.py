ad858139983d897587cf56abf76c9557cec6db7f
Jander Melo <janderwow@gmail.com>
2023-10-17 20:07:19 -0300

    resolucao do exercicio

--- aula8.py
+++ aula8.py
@@ -1,2 +1,17 @@
 # exercicio proposto
 # criacao de variaveis
+
+nome = 'Jander'
+sobrenome = 'Melo'
+idade = 33
+ano_nascimento = 1990
+maior_de_idade = idade >= 18
+altura_metros = 1.79
+
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de Nascimento: ', ano_nascimento)
+print('E maior de idade?: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
