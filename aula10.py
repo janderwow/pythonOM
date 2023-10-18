488cf026fdb8bd3a371e3c8a08c80adbfb465788
Jander Melo <janderwow@gmail.com>
2023-10-18 01:45:08 -0300

    concat e repet com op aritmetcs

---
+++ aula10.py
@@ -0,0 +1,8 @@
+# concatenacao e repeticao com operadores aritmeticos
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes) # output AAAAAAAAAA
+tres_vezes_jander = 3 * 'Jander'
+print(tres_vezes_jander) # output JanderJanderJander
