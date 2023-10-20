ee0ffd1ffd6b4aaa778b71ad3d49d4cca7371be8
Jander Melo <janderwow@gmail.com>
2023-10-19 23:36:39 -0300

    formatacao de strings com metodo format

--- aula14.py
+++ aula14.py
@@ -1,4 +1,11 @@
 a = 'A'
 b = 'B'
 c = 1.1
-formato = ''.format(a,b,c)
+#string = 'a={0} b={0} c={:.2f}' # acessando por idexacao
+#formato = string.format(a,b,c)
+
+string2 = 'a={nome2} b={nome2} c={nome1}' # acessando por parametro nomeado
+formato2 = string2.format(nome1=a, nome2=b, nome3=c) 
+
+#print(formato)
+print(formato2)
