bce8e3526f763e1b323f123d40a2badbff0eff79
Jander Melo <janderwow@gmail.com>
2023-10-20 00:49:55 -0300

    revisao fluxo condicional

---
+++ aula17.py
@@ -0,0 +1,15 @@
+# if / elif...../ else
+# se / se nao se/ se nao
+
+condicao_1 = False
+condicao_2 = False
+condicao_3 = True
+
+if condicao_1 == True:
+  print('Codigo condicao 1')
+elif condicao_2 == True:
+  print('Codigo condicao 2')
+elif condicao_3 == True:
+  print('Codigo condicao 3')
+else:
+  print('Outro codigo')
